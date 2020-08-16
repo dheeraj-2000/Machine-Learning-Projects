6ac6627d73dfc12550cc70aa33ed5afda8624ab2
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-16 21:41:44 +0530

    UPDATED

--- flask_app/request.py
+++ flask_app/request.py
@@ -3,7 +3,7 @@ import json
 
 url = 'http://0.0.0.0:5000/api/'
 
-data = [[14.34, 1.68, 2.7, 25.0, 98.0, 2.8, 1.31, 0.53, 2.7, 13.0, 0.57, 1.96, 660.0]]
+data = [[14.23,1.71,2.43,15.6,127.0,2.80,3.06,0.28,2.29,5.64,1.04,3.92,1065.0]]
 j_data = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 r = requests.post(url, data=j_data, headers=headers)
