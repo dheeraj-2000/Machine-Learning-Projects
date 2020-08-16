30d8e7d8b18e9b1f968104012340e362b0b305fb
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-16 21:58:04 +0530

    UPDATED

--- flask_app/Wine_Prediction/app.py
+++ flask_app/Wine_Prediction/app.py
@@ -15,6 +15,6 @@ def makecalc():
     return jsonify(prediction)
 
 if __name__ == '__main__':
-    modelfile = '/home/dheeraj/my_projects/my_project_env/practice/motion_detector/flask_app/final_prediction.pickle'
+    modelfile = '/home/dheeraj/my_projects/my_project_env/practice/motion_detector/flask_app/Wine_Prediction/final_prediction.pickle'
     model = p.load(open(modelfile, 'rb'))
     app.run(debug=True, host='0.0.0.0')
